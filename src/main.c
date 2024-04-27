5c284a3d46d76cc0828ea10292ad2a9c6387aad3
Colin Barker <colin.barker@biologic-tech.com>
2024-04-27 10:54:30 +0100

    EOS

--- src/main.c
+++ src/main.c
@@ -8,8 +8,6 @@
 
 #include <errno.h>
 #include <string.h>
-#include <zephyr/shell/shell.h>
-
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
@@ -18,7 +16,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/device.h>
 #include <zephyr/sys/util.h>
-#include <stdlib.h>
+
 
 #define STRIP_NODE        DT_NODELABEL(apa102)
 
@@ -79,55 +77,26 @@ int main(void)
     white_dimmer(50);
     size_t color = 0;
     int rc;
-    while (1){
-    // Main program loop
-        if (fire_rgb_on) {
-            for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
-                memset(&pixels, 0x00, sizeof(pixels));
-                memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
-
-                rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
-                if (rc) {
-                    LOG_ERR("couldn't update strip: %d", rc);
-                }
-
-                k_sleep(DELAY_TIME);
-            }
-
-            color = (color + 1) % ARRAY_SIZE(colors);
-        }
-    }
-    return 0;
-}
-
-void cmd_firergb_toggle(){
-
-    fire_rgb_on = !fire_rgb_on;
-}
-
-static int cmd_white_dimmer(const struct shell *shell, size_t argc, char **argv)
-{
-    if (argc != 2) {
-        shell_print(shell, "Invalid number of arguments");
-        return -EINVAL;
-    }
-
-    int percentage = atoi(argv[1]);
-
-    if (percentage <= 0 || percentage > 100) {
-        shell_print(shell, "Invalid percentage. It must be between 1 and 100.");
-        return -EINVAL;
-    }
-
-    white_dimmer(percentage);
-
-    return 0;
-}
-
-//SHELL_STATIC_SUBCMD_SET_CREATE(sub_white_dimmer,
-//                               SHELL_CMD_ARG(set_dimmer, NULL, "<percentage>", cmd_white_dimmer, 2, 0),
-////                               SHELL_CMD(rgb_toggle, NULL, "<Toggle RGB>", cmd_firergb_toggle),
-//                               SHELL_SUBCMD_SET_END
-//);
+//    while (1){
+//    // Main program loop
+//        if (fire_rgb_on) {
+//            for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
+//                memset(&pixels, 0x00, sizeof(pixels));
+//                memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
 //
-//SHELL_CMD_REGISTER(white_dimmer, &sub_white_dimmer, "Set white dimmer", NULL);
+//                rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+//                if (rc) {
+//                    LOG_ERR("couldn't update strip: %d", rc);
+//                }
+//
+//                k_sleep(DELAY_TIME);
+//            }
+//
+//            color = (color + 1) % ARRAY_SIZE(colors);
+//        }
+//    }
+    return 0;
+}
+
+
+
